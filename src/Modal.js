2530920c61cab06f106f8792c07e2070bfe3be4e
MBJ-97 <mehdiibeldjoudi@gmail.com>
2021-10-01 12:05:43 +0100

    click and copy button

--- src/Modal.js
+++ src/Modal.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 export default function Modal({onShowChange, shrLink}) {
+
+    const [copied, setCopied] = useState(false);
 
     const handleClick = () => {
         onShowChange(false);
     }
+
+    const clickNdCopy = () => {
+        var text = document.getElementById('textt').innerText;
+        var elem = document.createElement("textarea");
+        document.body.appendChild(elem);
+        elem.value = text;
+        elem.select();
+        document.execCommand("copy");
+        document.body.removeChild(elem);
+        setCopied(true);
+
+        console.log('it clicked')
+    }
+
     return ( 
         
         <div className="modal">
@@ -13,11 +30,15 @@ export default function Modal({onShowChange, shrLink}) {
                     <h4 className="modal-title">Sharing is caring</h4>
                 </div>
                 <div className="modal-body">
-                    {shrLink}
+                    <p className="paragraph" id="textt">{shrLink}</p>
                 </div>
                 <div className="modal-footer">
                     <button onClick ={handleClick} className="button">
+                        
                         Close
+                    </button>
+                    <button onClick={clickNdCopy} className="copy-btn button">
+                        {copied?("Copied!"):("Click'n'Copy")}
                     </button>
                 </div>
             </div>
